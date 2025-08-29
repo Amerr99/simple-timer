8fb11654e9263fd58ebf8094ced3fb933551cbae
Amer Melhem <amermelhem.99@icloud.com>
2025-08-29 03:21:52 +0300

    last edit

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,11 +1,18 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import TimerDisplay from "./TimerDisplay";
 import TimerControls from "./TimerControls";
 
 const Timer = () => {
   const timerRef = useRef(null);
-  const [timer, setTimer] = useState(0);
+  const [timer, setTimer] = useState(() => {
+    return Number(localStorage.getItem("timer") || 0);
+  });
   const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("timer", timer);
+  }, [timer]);
+
   const toggleTimer = () => {
     if (isRunning) {
       clearInterval(timerRef.current);
@@ -17,12 +24,15 @@ const Timer = () => {
     }
     setIsRunning(!isRunning);
   };
+
   const resetTimer = () => {
     clearInterval(timerRef.current);
     setIsRunning(false);
     setTimer(0);
     timerRef.current = null;
+    localStorage.removeItem("timer");
   };
+
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
       <TimerDisplay timer={timer} />
--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 
 function TimerControls({ toggleTimer, isRunning, resetTimer }) {
+  const startButtonRef = useRef(null);
+  useEffect(() => {
+    if (startButtonRef.current) {
+      startButtonRef.current.focus();
+    }
+  }, []);
   return (
     <div>
       <button
+        ref={startButtonRef}
         onClick={toggleTimer}
         className="mt-3 mr-4 bg-green-500 text-white px-4 py-2 rounded hover: bg-green-600"
       >
